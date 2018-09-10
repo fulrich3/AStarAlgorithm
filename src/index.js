efb79669a56a4911473906187695f018fe36e5a6
unknown <fulrich3@gmail.com>
2018-09-10 18:27:24 +0200

    add css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import _ from 'lodash';
+//import _ from 'lodash';
+import './style.css';
 
 function component() {
     let element = document.createElement('div');
   
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
+    element.innerHTML = "<p>test</p>";
+    element.classList.add('hello');
+
     return element;
 }
 
