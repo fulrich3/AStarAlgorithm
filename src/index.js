61cbfbd1f9e000f371f825d3098127988fd8e60b
unknown <fulrich3@gmail.com>
2018-09-12 17:18:33 +0200

    test different pages

--- src/index.js
+++ src/index.js
@@ -1,19 +1,2 @@
 //import _ from 'lodash';
 import './style.scss';
-import './template.html';
-
-
-function component() {
-    let element = document.createElement('div');
-  
-    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    element.innerHTML = '<p class="red">excellent</p>';
-    element.classList.add('hello');
-
-    return element;
-}
-
-document.body.appendChild(component());
-
-console.log("O haï Mark!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,60 @@
+// ============================
+// Dependencies
+// ============================
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var isProd = process.env.NODE_ENV === "production";
+
+// ============================
+// CSS configuration
+// ============================
 var cssDev = ["style-loader", "css-loader", "sass-loader"];
 var cssProd = ExtractTextPlugin.extract({ 
     fallback:'style-loader',
     use:['css-loader','sass-loader'],
 });
-
-
 var cssConfig = isProd ? cssProd : cssDev;
 
+// ============================
+// Webpack configuration
+// ============================
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        contact: './src/contact.js',
+    },
     output: {
-        filename: 'app.bundle.js',
+        //filename: 'app.bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
-        compress: true,
+        compress: false,
         stats: "errors-only",
     },
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "A Star Algorithm",
+            minify: {
+                collapseWhitespace: isProd,
+            },
+            hash: isProd,
+            template: "./src/templates/index.ejs",
+            filename: "index.html",
+        }),
+        new HtmlWebpackPlugin({
+            title: "Contact",
+            minify: {
+                collapseWhitespace: isProd,
+            },
+            hash: isProd,
+            template: "./src/templates/contact.ejs",
+            filename: "contact.html",
+        }),
         new ExtractTextPlugin({
             filename:'app.bundle.css',
             disable: !isProd,
