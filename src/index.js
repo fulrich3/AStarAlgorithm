38dee6c0569a8da638e7995b19b29f7334a5ccbe
unknown <fulrich3@gmail.com>
2018-09-12 15:47:08 +0200

    configuration dev & prod

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 //import _ from 'lodash';
-import './style.css';
+import './style.scss';
+import './template.html';
+
 
 function component() {
     let element = document.createElement('div');
   
     //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
-    element.innerHTML = "<p>test</p>";
+    element.innerHTML = '<p class="red">excellent</p>';
     element.classList.add('hello');
 
     return element;
 }
 
 document.body.appendChild(component());
+
+console.log("O haï Mark!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,52 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var isProd = process.env.NODE_ENV === "production";
+var cssDev = ["style-loader", "css-loader", "sass-loader"];
+var cssProd = ExtractTextPlugin.extract({ 
+    fallback:'style-loader',
+    use:['css-loader','sass-loader'],
+});
+
+
+var cssConfig = isProd ? cssProd : cssDev;
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'app.bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        stats: "errors-only",
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new ExtractTextPlugin({
+            filename:'app.bundle.css',
+            disable: !isProd,
+        }),
+    ],
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: [':data-src']
+                    }
+                }
+            },
+            {
+                test:/\.(s*)css$/,
+                use: cssConfig
+                //use:['style-loader','css-loader', 'sass-loader']
+            },
         ]
-    }
+    },
 };
